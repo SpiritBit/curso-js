1a2ac6d542775605b463dd17e31f2520e4c1d947
SpiritBit <adolfopa@outlook.es>
2023-11-07 16:57:39 +0100

    20231107-1657

--- part1/courseinfo/src/components/App.js
+++ part1/courseinfo/src/components/App.js
@@ -5,12 +5,14 @@ import Note from './Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('new note...')
+  const [showAll, setShowAll] = useState(true)
 
-  const noHay = 'No hay registros'
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
 
   const addNote = (event) => {
     event.preventDefault()
 
+    if (!newNote) { return }
 
     const noteObject = {
       id: notes.length + 1,
@@ -21,11 +23,9 @@ const App = (props) => {
 
     setNotes([...notes, noteObject])
     setNewNote('')
-
   }
 
   const handleNoteChange = (event) => {
-    console.log(`valor: ${event.target.value}`)
     if (event.target.value !== undefined) {
       setNewNote(event.target.value)
     }
@@ -35,8 +35,13 @@ const App = (props) => {
     <>
       <div>
         <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+        </div>
         <ul>
-          {notes.length > 0 ? notes.map(note => <Note key={note.id} note={note.content} />) : <p>{noHay}</p>}
+          {notesToShow.map(note => <Note key={note.id} note={note.content} />)}
         </ul>
       </div>
       <form onSubmit={addNote}>
